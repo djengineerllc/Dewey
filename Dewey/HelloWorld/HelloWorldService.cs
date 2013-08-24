24b4f2e56088084975eb10df480daf7dfd58b93d
djengineerllc <phillipmoore@gmail.com>
2013-08-24 11:18:55 -0500

    Setup Dewey

---
+++ Dewey/HelloWorld/HelloWorldService.cs
@@ -0,0 +1,21 @@
+﻿using ServiceStack.ServiceHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dewey.HelloWorld
+{
+    /// <summary>
+    /// Create your ServiceStack web service implementation.
+    /// </summary>
+    public class HelloWorldService : IService
+    {
+        public object Any(HelloWorld request)
+        {
+            //Looks strange when the name is null so we replace with a generic name.
+            var name = request.Name ?? "World!";
+            return new HelloWorldResponse { Result = "Hello " + name };
+        }
+    }
+}
---
+++ Dewey/HelloWorld/Model/HelloWorldResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dewey.HelloWorld
+{
+    /// <summary>
+    /// Define your ServiceStack web service response (i.e. Response DTO).
+    /// </summary>
+    public class HelloWorldResponse
+    {
+        public string Result { get; set; }
+    }
+}
